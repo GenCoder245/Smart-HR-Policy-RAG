fe3d4144d119154fe84286fa2fdf42880ba42df0
GenCoder245 <curiousegr156@gmail.com>
2026-07-30 16:33:32 +0530

    added code for data retrieval

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from src.ingestion.ingest_docs import ingest_data_directory
 import structlog
 from my_logging import configure_logging
 
-
 logger = structlog.get_logger()
 
 policy_settings : Settings = get_settings()
@@ -13,10 +12,15 @@ configure_logging(policy_settings.log_level)
 policy_retriever = PolicyRetriever(settings=get_settings())
 policy_retriever = policy_retriever.initialize()
 
-no_of_files, no_of_chunks, chunk_ids = ingest_data_directory(settings=policy_settings, retriever= policy_retriever)
+if policy_settings.re_ingest_docs:
+    logger.info(f"Re-ingesting documents into the vectordb.")
+    no_of_files, no_of_chunks, chunk_ids = ingest_data_directory(settings=policy_settings, retriever= policy_retriever)
+    logger.info(f"Inserted {no_of_chunks} no.of chunks into the vectordb.")
 
-logger.info(f"Inserted {no_of_chunks} no.of chunks into the vectordb.")
 
-
-
-
+# This part is just for validation purposes.
+user_query = "what is the performance management process followed here ?" # "how many sick leaves are there"
+retrieved_documents = policy_retriever.retrieve_documents(user_query)
+# Can also use below code
+#retrieved_documents = policy_retriever.search_with_score(user_query)
+logger.info(f"retrieved_documents: {retrieved_documents}")
